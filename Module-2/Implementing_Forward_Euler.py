49bdc29137d9ad86e2232a99e10bff4f7ffb9bb5
jenni4j <jenni4j@gmail.com>
2020-05-24 20:47:48 -0700

    added plot generation

--- Module-2/Implementing_Forward_Euler.py
+++ Module-2/Implementing_Forward_Euler.py
@@ -3,7 +3,6 @@ import numpy as np
 
 
 ##Functions to implement
-
 def ode_rhs(u,beta,kappa):
     s = u[0]
     i = u[1]
@@ -12,13 +11,17 @@ def ode_rhs(u,beta,kappa):
     didt = 0
     drdt = 0
     dudt = [dsdt,didt,drdt]
+    return dudt
 
+def forwardEulerUpdate(u,dt,beta,kappa):
+    u_next = 0
+    return u_next
 
-
+##For checking implementation of ODE RHS and Forward Euler
 i = 0.01
 s = 1 - i
 r = 0
-u0 = [s;i;r]
+u0 = [s,i,r]
 
 beta = 1/2 
 kappa = 1/3
@@ -38,5 +41,22 @@ for t in range(1,51):
     u_next = forwardEulerUpdate(u_now,dt,beta,kappa)
     u_check[:,t] = u_next
 
+##Generating plots for answering the multiple choice questions
+i = 0.01
+s = 1 - i
+r = 0
+u0 = [s,i,r]
+
+beta = 1/2
+kappa = 1/3
+
+dt = 1
+T = range(0,100,dt)
+
+u = np.zeros(3,len(t))
+u[:,0] = u0
+
+for t = 1:len(T)
+    u[:,t] = forwardEulerUpdate(u[:,t-1],dt,beta,kappa)
 
 
