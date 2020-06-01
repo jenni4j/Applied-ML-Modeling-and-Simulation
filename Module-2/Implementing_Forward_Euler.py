20fa153fdc9f36f239823b617788f22ed742dff2
jenni4j <jenni4j@gmail.com>
2020-05-31 22:32:51 -0700

    fix plot again

--- Module-2/Implementing_Forward_Euler.py
+++ Module-2/Implementing_Forward_Euler.py
@@ -24,32 +24,33 @@ s = 1 - i
 r = 0
 u0 = [s,i,r]
 
-beta = 1/2 
-kappa = 1/3
+beta = 0.5
+kappa = 0.33
 
 dudt = ode_rhs(u0, beta, kappa)
+
 
 dsdt = dudt[0]
 didt = dudt[1]
 drdt = dudt[2]
 
 dt = 1
-u_check = np.zeros([3,52])
-u_check[:,0] = u0
+u_check = np.zeros([3,3])
+u_check[0,:] = u0
 
-for t in range(1,52):
-    u_now = u_check[:,t-1]
+for t in range(1,2):
+    u_now = u_check[t-1,:]
     u_next = forwardEulerUpdate(u_now,dt,beta,kappa)
-    u_check[:,t] = u_next
+    u_check[t,:] = u_next
 
 ##Generating plots for answering the multiple choice questions
-i = 0.01
+i = 0.0001
 s = 1 - i
 r = 0
 u0 = [s,i,r]
 
-beta = 1/2
-kappa = 1/3
+beta = 0.5
+kappa = 0.33
 
 dt = 1
 T = np.arange(0,100,dt)
@@ -60,8 +61,9 @@ u[:,0] = u0
 for t in range(1,len(T)):
     u[:,t] = forwardEulerUpdate(u[:,t-1],dt,beta,kappa)
 
-#plt.plot(T,u[0,:], label = 's(t) Approx')
+plt.plot(T,u[0,:], label = 's(t) Approx')
 plt.plot(T,u[1,:], label = 'i(t) Approx')
-#plt.plot(T,u[2,:], label = 'r(t) Approx')
+plt.plot(T,u[2,:], label = 'r(t) Approx')
+plt.legend(loc='best')
 plt.show()
 
