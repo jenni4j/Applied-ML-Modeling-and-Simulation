4f736ae9cd0c50cef92ec5ef3378bf14ebfd7708
jenni4j <jenni4j@gmail.com>
2020-06-03 23:50:27 -0700

    Starting assignment 3

--- Module-3/Implementing_Left_Differencing.py
+++ Module-3/Implementing_Left_Differencing.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 N = 200
 
 h = 2*pi/N
@@ -8,4 +12,16 @@ A = zeros(N,N)
 
 for i in range(1,N):
     A[i,i] = 1/h
+
+A(1,N) = -1/h
+
+for i in range(1,N):
+    A(i,i-1) = -1/h
+
+spy(A(1:10,1:10))
+
+dudx = A*u
+
+
+
 
