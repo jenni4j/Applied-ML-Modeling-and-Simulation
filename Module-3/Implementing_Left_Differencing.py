5bf826b717a36dac4e749c52727be1b9b44eb582
jenni4j <jenni4j@gmail.com>
2020-06-08 23:55:30 -0700

    added plots

--- Module-3/Implementing_Left_Differencing.py
+++ Module-3/Implementing_Left_Differencing.py
@@ -3,24 +3,30 @@ import matplotlib.pyplot as plt
 
 
 N = 200
-
+#set up grid and compute state vector
 h = 2*np.pi/N
 x = np.transpose(np.linspace(0,2*np.pi,N+1))
 u = np.sin(x)
 
 A = np.zeros((N,N))
 
+#loop through and replace appropriate entries with -1/h
 for i in range(0,N):
     A[i,i] = 1/h
 
-A(1,N) = -1/h
+A[1,N] = -1/h
 
 for i in range(1,N):
-    A(i,i-1) = -1/h
+    A[i,i-1] = -1/h
 
-spy(A(1:10,1:10))
-
+#compute derivative
 dudx = A*u
+
+plt.plot(x,cos(x),'k')
+plt.plot(x,dudx)
+plt.legend(loc='best')
+plt.show()
+
 
 
 
