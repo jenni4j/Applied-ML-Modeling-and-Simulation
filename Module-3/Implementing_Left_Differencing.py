92c667834a8e3df96219c83c1d26379212b84bb7
jenni4j <jenni4j@gmail.com>
2020-06-11 23:57:30 -0700

    added dot product

--- Module-3/Implementing_Left_Differencing.py
+++ Module-3/Implementing_Left_Differencing.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-N = 200
+N = 2
 #set up grid and compute state vector
 h = 2*np.pi/N
 x = np.transpose(np.linspace(0,2*np.pi,N))
@@ -14,17 +14,19 @@ A = np.zeros((N,N))
 for i in range(0,N):
     A[i,i] = 1/h
 
-A[1,N-1] = -1/h
+A[0,N-1] = -1/h
 
 for i in range(1,N):
     A[i,i-1] = -1/h
 
 #compute derivative
-dudx = A*u
+dudx = np.dot(u,A)
 
-plt.plot(x,np.cos(x),'k')
-plt.plot(x,dudx)
+plt.plot(x,np.cos(x),label = 'Truth')
+plt.plot(x,dudx,label = 'Approximation')
 plt.legend(loc='best')
+plt.xlabel('x')
+plt.ylabel('dudx')
 plt.show()
 
 
