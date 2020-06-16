fe3bf212e6b9fcbb6cc38c7c2047a1c4a6f48adc
jenni4j <jenni4j@gmail.com>
2020-06-15 23:12:06 -0700

    added integration options

--- Module-3/Simulating_PDEs.py
+++ Module-3/Simulating_PDEs.py
@@ -1,5 +1,13 @@
+import numpy as np
 nSpace = 100 # of spatial grid points
 nTime = 100 # of time steps
 
-ux_scheme = 'left' #choose 'left' or 'right'
+ux_scheme = 'left' #choose 'left' or 'right' for type of discretiztion stencil
 
+#timestepper so we know which integrator to use
+# fe (forward Euler), be (backward Euler), rk2 (two stage Runge-Kutta), rk4 (four stage Runge-Kutta)
+
+timestepper = 'fe'
+x = np.linspace(0,1,nSpace+1)
+dx = 1/nSpace
+u0 = init(x)
