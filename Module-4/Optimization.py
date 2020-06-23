d5d8b255607503785c2f413ce63e17075c9449f6
jenni4j <jenni4j@gmail.com>
2020-06-22 23:51:04 -0700

    adding gradient descent

--- Module-4/Optimization.py
+++ Module-4/Optimization.py
@@ -4,3 +4,13 @@ def grad(x):
     g = [g1,g2]
     return g
 
+def grad_descent(g, alpha, x0, max_iter, grad_tol):
+    x = x0
+    grad = g(x)
+
+    x_iter = x0
+    g_iter = norm(grad)
+
+    counter = 1
+
+    
