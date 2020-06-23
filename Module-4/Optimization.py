b4c7cc0ac9aa23c4594e1c9e258120b53205a8fe
jenni4j <jenni4j@gmail.com>
2020-06-22 23:54:45 -0700

    finish gd

--- Module-4/Optimization.py
+++ Module-4/Optimization.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def grad(x):
     g1 = 1/(1-x(1)-x(2))-1/x(1)
     g2 = 1/(1-x(1)-x(2))-1/x(2)
@@ -9,8 +11,14 @@ def grad_descent(g, alpha, x0, max_iter, grad_tol):
     grad = g(x)
 
     x_iter = x0
-    g_iter = norm(grad)
+    g_iter = np.linalg.norm(grad)
 
     counter = 1
 
-    
+    while(np.linalg.norm(grad) > grad_tol && counter < max_iter):
+        x = x - alpha*grad
+        x_iter = [x_iter, x]
+        grad = g(x)
+        g_iter = [g_iter, np.linalg.norm(grad)]
+        counter = counter + 1
+        
